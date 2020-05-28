b416750b7150f49bec9fd679b040ffb7f0640c0f
Alessandro Sebastiani <sebbalex@gmail.com>
2020-05-29 00:56:14 +0200

    initial push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Container from "./Container";
+import Counter from "./Counter";
+
+const initialData = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+  { id: 3, value: 0 }
+];
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Container />
+    <Counter />
   </React.StrictMode>,
   rootElement
 );
+
--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,38 @@
 import React from "react";
 import "./styles.css";
 
-const Counter = props => {
-  const handleIncrement = e => {
-    props.incrementCount(props.elemID);
-  };
-  const handleDecrement = e => {
-    props.decrementCount(props.elemID);
-  };
-  return (
-    <div className="App">
-      <h1>{props.count}</h1>
-      <button className="btn" onClick={handleIncrement}>
-        Increment
-      </button>
-      <button className="btn" onClick={handleDecrement}>
-        Decrement
-      </button>
-    </div>
-  );
-};
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.incrementCount = this.incrementCount.bind(this);
+    this.decrementCount = this.decrementCount.bind(this);
+  }
+  incrementCount() {
+    this.setState(state => ({
+      count: state.count + 1
+    }));
+  }
+  decrementCount() {
+    this.setState(state => ({
+      count: state.count - 1
+    }));
+  }
 
-export default Counter;
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.count}</h1>
+        <button className="btn" onClick={this.incrementCount}>
+          Increment
+        </button>
+        <button className="btn" onClick={this.decrementCount}>
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
+
